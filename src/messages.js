9f7ac0b15961281b4d45ccea689c87ae23176898
James Muscat <jamesremuscat@gmail.com>
2021-10-19 22:10:22 +0100

    Complete flag message generator.

--- src/messages.js
+++ src/messages.js
@@ -1,3 +1,5 @@
+import { FlagState } from './racing';
+
 class Message {
   constructor(category, message, style) {
     this.category = category;
@@ -16,14 +18,28 @@ const FlagMessage = (manifest, oldState, newState) => {
 
   const newFlag = newState.session?.flagState;
 
-  if (oldState.session?.flagState !== newFlag) {
+  if (oldState.session?.flagState && oldState.session.flagState !== newFlag) {
     switch(newFlag) {
-      case 'GREEN':
+      case FlagState.GREEN:
         return new Message('Track', 'Green flag - track clear', 'green');
-      case 'RED':
+      case FlagState.SC:
+        return new Message('Track', 'Safety car deployed', 'yellow');
+      case FlagState.VSC:
+        return new Message('Track', 'Virtual safety car deployed', 'yellow');
+      case FlagState.YELLOW:
+        return new Message('Track', 'Yellow flags shown', 'yellow');
+      case FlagState.FCY:
+        return new Message('Track', 'Full course yellow', 'yellow');
+      case FlagState.CAUTION:
+        return new Message('Track', 'Full course caution', 'yellow');
+      case FlagState.RED:
         return new Message('Track', 'Red flag', 'red');
-      case 'CHEQUERED':
+      case FlagState.CHEQUERED:
         return new Message("Track", "Chequered flag", "track");
+      case FlagState.CODE_60:
+        return new Message("Track", "Code 60", "code60");
+      case FlagState.WHITE:
+        return new Message("Track", "White flag - final lap", "white");
       default:
     }
   }

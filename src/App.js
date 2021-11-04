3cd43e4f17459f63500e1333c91a0b9e5f04e35e
James Muscat <jamesremuscat@gmail.com>
2021-11-04 10:42:48 +0000

    Store and retrieve user settings from extension.
    This not only persists the settings, but also syncs them between browser
    instances.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from 'styled-components';
 
 import { PluginDetector } from './modules/pluginBridge';
 
@@ -8,40 +9,43 @@ import { Start } from './pages/Start';
 import { Timing } from './pages/Timing';
 import { SettingsProvider } from './modules/settings';
 import { ThemeSettingsProvider } from './components/ThemeSettingsProvider';
+import { Theme } from './theme';
 
 function App() {
   return (
-    <SettingsProvider>
-      <ThemeSettingsProvider>
-        <HelmetProvider>
-          <Helmet
-            defaultTitle='Timing71 Beta'
-            titleTemplate='%s — Timing71 Beta'
-          />
-          <BrowserRouter>
-            <Switch>
-              <Route
-                component={Home}
-                exact
-                path='/'
-              />
-              <PluginDetector>
-                <Switch>
-                  <Route
-                    component={Start}
-                    path='/start'
-                  />
-                  <Route
-                    component={Timing}
-                    path='/timing/:serviceUUID'
-                  />
-                </Switch>
-              </PluginDetector>
-            </Switch>
-          </BrowserRouter>
-        </HelmetProvider>
-      </ThemeSettingsProvider>
-    </SettingsProvider>
+    <ThemeProvider theme={Theme}>
+      <HelmetProvider>
+        <Helmet
+          defaultTitle='Timing71 Beta'
+          titleTemplate='%s — Timing71 Beta'
+        />
+        <BrowserRouter>
+          <Switch>
+            <Route
+              component={Home}
+              exact
+              path='/'
+            />
+            <PluginDetector>
+              <SettingsProvider>
+                <ThemeSettingsProvider>
+                  <Switch>
+                    <Route
+                      component={Start}
+                      path='/start'
+                    />
+                    <Route
+                      component={Timing}
+                      path='/timing/:serviceUUID'
+                    />
+                  </Switch>
+                </ThemeSettingsProvider>
+              </SettingsProvider>
+            </PluginDetector>
+          </Switch>
+        </BrowserRouter>
+      </HelmetProvider>
+    </ThemeProvider>
   );
 }
 
--- src/modules/settings/context.js
+++ src/modules/settings/context.js
@@ -1,4 +1,5 @@
-import { createContext, useCallback, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { PluginContext } from "../pluginBridge";
 
 const DEFAULT_SETTINGS = {
   animation: true,
@@ -10,12 +11,30 @@ const SettingsContext = createContext();
 
 export const SettingsProvider = ({ children }) => {
   const [settings, setSettings] = useState(DEFAULT_SETTINGS);
+  const port = useContext(PluginContext);
+
+  useEffect(
+    () => {
+      port.send({ type: 'RETRIEVE_SETTINGS' }).then(
+        ({ settings }) => {
+          setSettings({ ...DEFAULT_SETTINGS, ...settings });
+        }
+      );
+    },
+    [port]
+  );
 
   const updateSettings = useCallback(
-    (newSettings) => setSettings(
-      oldSettings => ({ ...oldSettings, ...newSettings })
-    ),
-    []
+    (newSettings) => {
+      setSettings(
+        oldSettings => {
+          const saveableSettings = { ...oldSettings, ...newSettings };
+          port.send({ type: 'STORE_SETTINGS', settings: saveableSettings });
+          return saveableSettings;
+        }
+      );
+    },
+    [port]
   );
 
   return (

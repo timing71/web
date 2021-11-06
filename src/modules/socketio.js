c7060919bf4ea397abcb6820ddfbc4ccbee88736
James Muscat <jamesremuscat@gmail.com>
2021-11-06 15:33:41 +0000

    Better SocketIO polling behaviour.

--- src/modules/socketio.js
+++ src/modules/socketio.js
@@ -28,6 +28,42 @@ function yeast() {
   return now + '.' + encode(seed++);
 }
 
+const splitPayload = (data) => {
+  // Based on https://github.com/socketio/engine.io-parser/blob/2.2.x/lib/browser.js#L371
+  const messages = [];
+  let length = '', n, msg;
+
+  for (var i = 0, l = data.length; i < l; i++) {
+    var chr = data.charAt(i);
+
+    if (chr !== ':') {
+      length += chr;
+      continue;
+    }
+
+    if (length === '' || (length !== (n = Number(length)))) {
+      // parser error - ignoring payload
+      return messages;
+    }
+
+    msg = data.substr(i + 1, n);
+
+    if (length !== msg.length) {
+      // parser error - ignoring payload
+      return messages;
+    }
+
+    if (msg.length) {
+      messages.append(msg);
+    }
+
+    // advance cursor
+    i += n;
+    length = '';
+  }
+  return messages;
+};
+
 export const useSocketIo = (host, uuid, callback) => {
   const port = useContext(PluginContext);
   const socketURL = `${host}/socket.io/?EIO=4`;
@@ -41,16 +77,21 @@ export const useSocketIo = (host, uuid, callback) => {
 
       let sid = null;
 
+      let ws = null;
+      let pingInterval = null;
+      let polling = true;
+
       const decoder = new Decoder();
       decoder.on('decoded', (packet) => {
         if (packet.type === 4 && packet.data) {
           const [event, data] = packet.data;
           callback && callback(event, data);
         }
+        else if (packet.type === 3) {
+          // Successful response from websocket
+          polling = false;
+        }
       });
-
-      let ws = null;
-      let pingInterval = null;
 
       port.fetch(pollingUrl).then(
         firstPollData => {
@@ -59,10 +100,27 @@ export const useSocketIo = (host, uuid, callback) => {
 
             sid = encodeURIComponent(data.sid);
 
+            const doPoll = () => {
+              if (polling) {
+                port.fetch(`${pollingUrl}&sid=${sid}&t=${yeast()}`).then(
+                  pollData => {
+                    splitPayload(pollData).forEach(
+                      msg => decoder.add(msg)
+                    );
+                    setTimeout(doPoll, 1000);
+                  }
+                );
+              }
+            };
+            doPoll();
+
             if ((data.upgrades || []).includes('websocket')) {
               // Technically we should include the sid in the websocket URL;
               // but I don't want to reimplement socket.io's session handling...
               ws = new WrappedWebsocket(wsUrl, port, uuid);
+              ws.on('open', () => {
+                ws.send('2probe');
+              });
               ws.on('message', data => decoder.add(data));
               pingInterval = window.setInterval(
                 () => ws.send('2'),
@@ -72,15 +130,7 @@ export const useSocketIo = (host, uuid, callback) => {
             }
             else {
               // No websockets available
-              const doPoll = () => {
-                port.fetch(`${pollingUrl}&sid=${sid}&t=${yeast()}`).then(
-                  pollData => {
-                    decoder.add(pollData.slice(pollData.indexOf(':') + 1));
-                    doPoll();
-                  }
-                );
-              };
-              doPoll();
+
             }
           }
           catch (e) {

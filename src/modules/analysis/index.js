191ac4914d685e4909b4b8302b3ca0b1f5833c20
James Muscat <jamesremuscat@gmail.com>
2021-11-26 18:09:54 +0000

    Add Cars to main analyser.

--- src/modules/analysis/index.js
+++ src/modules/analysis/index.js
@@ -1,20 +1,24 @@
 import { getSnapshot, types } from 'mobx-state-tree';
+import { Cars } from './cars';
 
 import { Messages } from './Messages';
 import { Session } from './Session';
 
 const Analyser = types.model({
+  cars: Cars,
   messages: Messages,
   session: Session
 }).actions(
   self => ({
     updateState(oldState, newState) {
+      self.cars.update(oldState, newState);
       self.messages.update(oldState, newState);
       self.session.update(oldState, newState);
       return getSnapshot(self);
     },
 
     reset() {
+      self.cars.reset();
       self.messages.reset();
       self.session.reset();
     }

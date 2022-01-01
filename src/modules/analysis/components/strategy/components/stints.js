48bff959ae92cfad338789eff3f2ec0191991f95
James Muscat <jamesremuscat@gmail.com>
2022-01-01 19:56:22 +0000

    Show best and mean laptimes per stint.

--- src/modules/analysis/components/strategy/components/stints.js
+++ src/modules/analysis/components/strategy/components/stints.js
@@ -1,6 +1,8 @@
 import { darken } from 'polished';
 import styled from 'styled-components';
 
+import dayjs from '../../../../../datetime';
+
 const StintBox = styled.rect.attrs({
   x: 3,
   y: 2,
@@ -50,11 +52,34 @@ const LapCount = styled(StintText).attrs({
   textAnchor: 'end'
 })``;
 
+const Best = styled(StintText).attrs({
+  x: 10,
+  dominantBaseline: 'auto',
+  textAnchor: 'start'
+})`
+  clip-path: ${ props => `polygon(0 0, 0 18px, ${props.overallWidth / 2}px 18px, ${props.overallWidth / 2}px 0)` };
+`;
+
+const Mean = styled(StintText).attrs({
+  dominantBaseline: 'auto',
+  textAnchor: 'end'
+})``; // Can't see a way of getting clip-path to work with textAnchor: 'end'
+
+const sum = (acc, val) => acc + val;
+
 const Stint = ({ height, stint, xScale }) => {
 
   const laps = stint.inProgress ? 1 + stint.car.currentLap - stint.startLap : stint.durationLaps;
 
   const width = Math.max(2, xScale(laps) - 6);
+
+  const relevantLaps = stint.laps.slice(1, -1); // ignore out and in laps
+
+  const best = relevantLaps.length > 0 ? Math.min(
+    ...relevantLaps.map(l => l.laptime)
+  ) : null;
+
+  const mean = relevantLaps.length > 0 ? (relevantLaps.map(l => l.laptime).reduce(sum, 0) / relevantLaps.length).toFixed(3) : null;
 
   return (
     <g transform={`translate(${xScale(stint.startLap - 1)}, 0)`}>
@@ -70,6 +95,19 @@ const Stint = ({ height, stint, xScale }) => {
       <LapCount x={width - 8}>
         {laps} lap{laps === 1 ? '' : 's'}
       </LapCount>
+      <Best
+        overallWidth={width}
+        y={height - 12}
+      >
+        Best {best ? dayjs.duration(best * 1000).format("m:ss.SSS") : '-'}
+      </Best>
+      <Mean
+        overallWidth={width}
+        x={width - 8}
+        y={height - 12}
+      >
+        Ave {mean ? dayjs.duration(mean * 1000).format("m:ss.SSS") : '-'}
+      </Mean>
     </g>
   );
 };

796ee5a9c48ed0e04969050860242a7e8735c132
James Muscat <jamesremuscat@gmail.com>
2022-01-04 22:00:43 +0000

    HTML is just better than SVG for this sort of thing.

---
+++ src/modules/analysis/components/strategy/components/StintBox.js
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import { animated } from '@react-spring/web';
+import dayjs from 'dayjs';
+import { darken } from 'polished';
+
+import { FlagState } from '../../../../../racing';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 0.25em;
+
+  background: ${
+    props => (
+      props.car && darken(
+        props.inProgress ? 0.25 : 0.35,
+        props.theme.classColours[(props.car.raceClass || '').toLowerCase().replace(/[-/ ]/, '')] || '#808080'
+      )
+    )
+  };
+
+  &:hover {
+    background: ${
+      props => (
+        props.car && darken(
+          props.inProgress ? 0.15 : 0.25,
+          props.theme.classColours[(props.car.raceClass || '').toLowerCase().replace(/[-/ ]/, '')] || '#808080'
+        )
+      )
+    };
+  }
+
+  transition: fill 0.15s ease-in-out;
+
+  user-select: none;
+  cursor: pointer;
+`;
+
+const Line = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1 1 50%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 0.5em;
+  overflow: hidden;
+`;
+
+const Text = styled.div`
+  color: white;
+  font-family: ${ props => props.theme.site.headingFont };
+  font-weight: ${ props => props.bold ? 'bold' : 'normal' };
+  text-align: ${ props => props.right ? 'right' : 'left' };
+  flex: 0 1 0;
+  white-space: nowrap;
+
+  &:first-child {
+    margin-right: 0.25em;
+  }
+`;
+
+const sum = (acc, val) => acc + val;
+
+export const StintBox = animated(
+  ({ height, stint, width, x }) => {
+
+    const laps = stint.inProgress ? stint.car.currentLap - stint.startLap : stint.durationLaps;
+
+    const relevantLaps = stint.laps.slice(1, stint.inProgress ? undefined : -1); // ignore out and in laps
+
+    const best = relevantLaps.length > 0 ? Math.min(
+      ...relevantLaps.map(l => l.laptime)
+    ) : null;
+
+    const mean = relevantLaps.length > 0 ?
+      (relevantLaps.filter(l => l.flag === FlagState.GREEN).map(l => l.laptime).reduce(sum, 0) / relevantLaps.length).toFixed(3)
+    : null;
+
+    return (
+      <foreignObject
+        height={height}
+        width={width}
+        x={x}
+      >
+        <Container car={stint.car}>
+          <Line>
+            <Text bold>{stint.driver.name}</Text>
+            <Text
+              bold
+              right
+            >
+              {laps} lap{laps === 1 ? '' : 's'}
+            </Text>
+          </Line>
+          <Line>
+            <Text>Best {best ? dayjs.duration(Math.round(best * 1000)).format("m:ss.SSS") : '-'}</Text>
+            <Text right>Ave {mean ? dayjs.duration(Math.round(mean * 1000)).format("m:ss.SSS") : '-'}</Text>
+          </Line>
+        </Container>
+      </foreignObject>
+    );
+  }
+);
--- src/modules/analysis/components/strategy/components/stints.js
+++ src/modules/analysis/components/strategy/components/stints.js
@@ -1,130 +1,7 @@
-import { darken } from 'polished';
-import styled from 'styled-components';
-import { Text } from '@visx/text';
 import { animated, useTransition } from '@react-spring/web';
 import { useMotionConfig } from '@nivo/core';
 
-import dayjs from '../../../../../datetime';
-
-const StintBox = styled.rect.attrs({
-  x: 3,
-  y: 2,
-  rx: 3
-})`
-  fill: ${
-    props => (
-      props.car && darken(
-        props.inProgress ? 0.25 : 0.35,
-        props.theme.classColours[(props.car.raceClass || '').toLowerCase().replace(/[-/ ]/, '')] || '#808080'
-      )
-    )
-  };
-
-  &:hover {
-    fill: ${
-      props => (
-        props.car && darken(
-          props.inProgress ? 0.15 : 0.25,
-          props.theme.classColours[(props.car.raceClass || '').toLowerCase().replace(/[-/ ]/, '')] || '#808080'
-        )
-      )
-    };
-  }
-
-  transition: fill 0.15s ease-in-out;
-
-  user-select: none;
-  cursor: pointer;
-`;
-
-const StintText = styled(Text).attrs({
-  scaleToFit: 'shrink-only'
-})`
-  fill: white;
-  font-family: ${ props => props.theme.site.headingFont };
-  pointer-events: none;
-`;
-
-const DriverName = styled(StintText).attrs(props => ({
-  x: 10,
-  y: 10,
-  dominantBaseline: 'hanging',
-  style: {
-    clipPath: `polygon(0 0, 0 18px, ${props.overallWidth / 2}px 18px, ${props.overallWidth / 2}px 0)`
-  },
-  overallWidth: undefined
-}))`
-  font-weight: bold;
-`;
-
-const LapCount = styled(StintText).attrs({
-  y: 10,
-  dominantBaseline: 'hanging',
-  textAnchor: 'end'
-})``;
-
-const Best = styled(StintText).attrs({
-  x: 10,
-  dominantBaseline: 'auto',
-  textAnchor: 'start'
-})``;
-
-const Mean = styled(StintText).attrs({
-  dominantBaseline: 'auto',
-  textAnchor: 'end'
-})``;
-
-const sum = (acc, val) => acc + val;
-
-const Stint = ({ height, stint, width, x }) => {
-
-  const laps = stint.inProgress ? stint.car.currentLap - stint.startLap : stint.durationLaps;
-
-  const relevantLaps = stint.laps.slice(1, stint.inProgress ? undefined : -1); // ignore out and in laps
-
-  const best = relevantLaps.length > 0 ? Math.min(
-    ...relevantLaps.map(l => l.laptime)
-  ) : null;
-
-  const mean = relevantLaps.length > 0 ? (relevantLaps.map(l => l.laptime).reduce(sum, 0) / relevantLaps.length).toFixed(3) : null;
-
-  return (
-    <g transform={`translate(${x}, 0)`}>
-      <StintBox
-        car={stint.car}
-        height={height - 4}
-        inProgress={stint.inProgress}
-        width={width}
-      />
-      <DriverName
-        overallWidth={width}
-      >
-        { stint.driver.name }
-      </DriverName>
-      <LapCount
-        width={width / 2}
-        x={width - 8}
-      >
-        {`${laps} lap${laps === 1 ? '' : 's'}`}
-      </LapCount>
-      <Best
-        width={width / 2}
-        y={height - 12}
-      >
-        {`Best ${best ? dayjs.duration(Math.round(best * 1000)).format("m:ss.SSS") : '-'}`}
-      </Best>
-      <Mean
-        width={width / 2}
-        x={width - 8}
-        y={height - 12}
-      >
-        {`Ave ${mean ? dayjs.duration(Math.round(mean * 1000)).format("m:ss.SSS") : '-'}`}
-      </Mean>
-    </g>
-  );
-};
-
-const AnimatedStint = animated(Stint);
+import { StintBox as SB } from './StintBox';
 
 export const CarStints = ({ height, stints, widthFunc, xFunc }) => {
 
@@ -149,7 +26,7 @@ export const CarStints = ({ height, stints, widthFunc, xFunc }) => {
       {
         transition(
           (style, stint) => (
-            <AnimatedStint
+            <SB
               height={height}
               stint={stint}
               x={xFunc(stint)}

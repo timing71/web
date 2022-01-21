325a15e5ff1aab90232590d1c7503a4388c01733
James Muscat <jamesremuscat@gmail.com>
2022-01-21 18:28:40 +0000

    Improve Debouncer.
    Not sure why `setInterval` was calling its callback more often than the
    specified interval... but let's work around that with a manual check.

--- src/components/Debouncer.js
+++ src/components/Debouncer.js
@@ -1,16 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import { ServiceStateContext, useServiceState } from "./ServiceContext";
 
-export const Debouncer = ({ children }) => {
+export const Debouncer = ({ children, minInterval=1000 }) => {
   const { state, updateState } = useServiceState();
   const [debouncedState, setDebouncedState] = useState(state);
   const pendingState = useRef(state);
   const pendingHighlights = useRef([]);
+  const lastUpdate = useRef(0);
+  const hasUpdated = useRef(false);
 
   useEffect(
     () => {
       pendingState.current = state;
       pendingHighlights.current = [...pendingHighlights.current, ...(state.highlight || [])];
+      hasUpdated.current = true;
     },
     [state]
   );
@@ -19,20 +22,27 @@ export const Debouncer = ({ children }) => {
     () => {
       const interval = window.setInterval(
         () => {
-          setDebouncedState({
-            ...pendingState.current,
-            highlight: [...pendingHighlights.current]
-          });
-          pendingHighlights.current = [];
+          if (hasUpdated.current) {
+            const now = Date.now();
+            if (now - lastUpdate.current >= minInterval) {
+              setDebouncedState({
+                ...pendingState.current,
+                highlight: [...pendingHighlights.current]
+              });
+              pendingHighlights.current = [];
+              lastUpdate.current = now;
+              hasUpdated.current = false;
+            }
+          }
         },
-        1000
+        minInterval / 2
       );
 
       return () => {
         window.clearInterval(interval);
       };
     },
-    []
+    [minInterval]
   );
 
   return (

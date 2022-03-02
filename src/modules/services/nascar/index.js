57d3e4db98299e0ce07ee8f3e463cb86404db2da
James Muscat <jamesremuscat@gmail.com>
2022-03-02 22:41:49 +0000

    Migrate NASCAR service.

--- src/modules/services/nascar/index.js
+++ src/modules/services/nascar/index.js
@@ -1,46 +1,25 @@
-import { useCallback, useContext, useEffect } from "react";
-import { useServiceManifest, useServiceState } from "../../../components/ServiceContext";
+import { HTTPPollingService } from '../service';
 
-import { PluginContext } from "../../pluginBridge";
 import { getManifest, translate } from "./translate";
 
 const DATA_URL = 'https://cf.nascar.com/live/feeds/live-feed.json';
 
-export const Nascar = () => {
+export class Nascar extends HTTPPollingService {
+  constructor(...args) {
+    super(
+      DATA_URL,
+      5000,
+      ...args
+    );
+  }
 
-  const port = useContext(PluginContext);
+  handleResponse(message) {
+    const jsonData = JSON.parse(message);
+    this.onManifestChange(getManifest(jsonData));
+    this.onStateChange(translate(jsonData));
 
-  const { updateManifest } = useServiceManifest();
-  const { updateState } = useServiceState();
+  }
 
-  const getData = useCallback(
-    () => {
-      port.fetch(DATA_URL).then(
-        message => {
-          const jsonData = JSON.parse(message);
-
-          const newManifest = getManifest(jsonData);
-          updateManifest(newManifest);
-          updateState(translate(jsonData));
-        }
-      );
-    },
-    [port, updateManifest, updateState]
-  );
-
-  useEffect(
-    () => {
-      getData();
-      const interval = window.setInterval(getData, 5000);
-
-      return () => {
-        window.clearInterval(interval);
-      };
-    },
-    [getData]
-  );
-
-  return null;
-};
+}
 
 Nascar.regex = /.*\.nascar\.com/;

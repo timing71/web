f626a43eb2a9efef2cc8089f060943893b4daca5
James Muscat <jamesremuscat@gmail.com>
2022-12-29 18:05:25 +0000

    Synthesise a `lastUpdated` timestamp.

--- src/pages/HostedTiming.js
+++ src/pages/HostedTiming.js
@@ -1,7 +1,11 @@
 import LZString from "lz-string";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useRouteMatch } from "react-router-dom";
+import { LoadingScreen } from "../components/LoadingScreen";
 import { ServiceManifestContext, ServiceStateContext } from "../components/ServiceContext";
 import { useSubscription } from "../modules/autobahn";
+import { DelayIndicator, DelaySetting, Menu, MenuBar, MenuSeparator, Spacer, SystemMessage, UpdateTime, ViewSettings, WallClock } from "../modules/menu";
 import { TimingScreen } from "../modules/timingScreen";
 
 export const HostedTiming = () => {
@@ -10,20 +14,56 @@ export const HostedTiming = () => {
   const manifest = services?.find(s => s.uuid === uuid);
 
   const compressedServiceState = useSubscription(`livetiming.service.${uuid}`, { get_retained: true });
+  const [lastUpdated, setLastUpdated] = useState();
+
+  useEffect(
+    // Legacy state doesn't include a lastUpdated timestamp
+    // We splat serviceState after we include it so that we don't squash any
+    // timestamp if it does appear in the upstream state.
+    () => {
+      setLastUpdated(Date.now());
+    },
+    [compressedServiceState]
+  );
 
   if (compressedServiceState && manifest) {
     const serviceState = JSON.parse(LZString.decompressFromUTF16(compressedServiceState));
 
     return (
       <ServiceManifestContext.Provider value={{ manifest }}>
-        <ServiceStateContext.Provider value={{ state: { ...serviceState, manifest } }}>
-          <TimingScreen></TimingScreen>
+        <ServiceStateContext.Provider value={{ state: { lastUpdated, ...serviceState, manifest } }}>
+          <TimingScreen>
+            <MenuBar>
+              <WallClock />
+              <UpdateTime />
+              <Spacer />
+              <SystemMessage />
+              <DelayIndicator />
+              {
+                process.env.NODE_ENV === 'development' && <span>[DEV]</span>
+              }
+              <Menu>
+                <DelaySetting />
+                <MenuSeparator />
+                {/* <ToggleMenuItem onClick={openAnalysis}>
+                  <span>
+                    <StackedBarChart size={24} />
+                  </span>
+                  <label>Launch analysis</label>
+                </ToggleMenuItem>
+                <MenuSeparator /> */}
+                <ViewSettings />
+                {/* <MenuSeparator />
+                <DownloadReplay /> */}
+              </Menu>
+            </MenuBar>
+          </TimingScreen>
         </ServiceStateContext.Provider>
       </ServiceManifestContext.Provider>
     );
 
   }
   else {
-    return <p>Loading...</p>;
+    return <LoadingScreen message='Loading timing data...' />;
   }
 };

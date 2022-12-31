7e9d180b99b696ff76e3cd56a5f087801d2e153c
James Muscat <jamesremuscat@gmail.com>
2022-12-31 17:35:07 +0000

    These need default exports to work with lazy import.

--- src/pages/HostedAnalysis.js
+++ src/pages/HostedAnalysis.js
@@ -95,3 +95,5 @@ export const HostedAnalysis = () => {
     );
   }
 };
+
+export default HostedAnalysis;
--- src/pages/HostedTiming.js
+++ src/pages/HostedTiming.js
@@ -94,3 +94,5 @@ export const HostedTiming = () => {
     return <LoadingScreen message='Loading timing data...' />;
   }
 };
+
+export default HostedTiming;
